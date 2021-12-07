f712811b8487aa99ef8d2fae0983eb9ccf458b46
Jeffrey Zalischi <jay.zalischi@gmail.com>
2021-12-07 16:41:24 -0500

    added song time information
    moved audio tag and useRef hook to app.js. Added get time function to determine overall duration. Also got currentTime of the song selected. Using the timeUpdateHandler as a value in onTimeUpdate whenever the audio tag updates the songInfo state updates.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 //COMPONENTS
 import Song from './components/Song';
 import Player from './components/Player';
@@ -9,6 +9,8 @@ import './styles/app.scss';
 import lofiPlaylist from './lofiPlaylist';
 
 function App() {
+  //Ref
+  const audioRef = useRef(undefined);
   //STATE
   //This State is used to hold playlist data
   const [songs, setSongs] = useState(lofiPlaylist());
@@ -16,6 +18,32 @@ function App() {
   const [currentSong, setCurrentSong] = useState(songs[0]);
   //This State determines if the song selected is playing
   const [isPlaying, setIsPlaying] = useState(false);
+  //Information of song Selected
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+
+  //EVENT HANDLER
+  //Adds values to setSongInfo state
+  const timeUpdateHandler = (e) => {
+    //Get Values from audio tag when it starts playing
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    //Round numbers to hundredths from millionths
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    //Used to determine place in song bar
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
+  };
 
   return (
     <div className='App'>
@@ -25,7 +53,15 @@ function App() {
         currentSong={currentSong}
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
+        audioRef={audioRef}
+        songInfo={songInfo}
+        setSongInfo={setSongInfo}
       />
+      <audio
+        ref={audioRef}
+        src={currentSong.audio}
+        onTimeUpdate={timeUpdateHandler}
+      ></audio>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlay,
@@ -7,10 +6,14 @@ import {
   faPause,
 } from '@fortawesome/free-solid-svg-icons';
 
-const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-  //ref
-  const audioRef = useRef(null);
-
+const Player = ({
+  currentSong,
+  isPlaying,
+  setIsPlaying,
+  audioRef,
+  songInfo,
+  setSongInfo,
+}) => {
   //Event Handlers
   //This function is used to Play currentSong selected
   const playSongHandler = () => {
@@ -23,8 +26,36 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     }
   };
 
+  //The function is used to convert the duration value into time
+  //Ex. 100seconds = 1min and 40seconds || 1:40
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
+  //Must add onChange handler to avoid input error
+  //When there is a change made to the audio bar input tag the songInfo state updates the currentTime
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
   return (
     <div className='player'>
+      <div className='time-control'>
+        <p>{getTime(songInfo.currentTime)}</p>
+        <div className='track'>
+          <input
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            type='range'
+            onChange={dragHandler}
+          />
+          <div className='animate-track'></div>
+        </div>
+        <p>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
+      </div>
       <div className='play-control'>
         <FontAwesomeIcon className='skip-left' icon={faAngleLeft} size='2x' />
         <FontAwesomeIcon
@@ -38,7 +69,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
           icon={faAngleRight}
           size='2x'
         />
-        <audio ref={audioRef} src={currentSong.audio}></audio>
       </div>
     </div>
   );
