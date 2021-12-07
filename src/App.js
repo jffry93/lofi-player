2f1f367018492f6f05e8866f07c950a0e97bba56
Jeffrey Zalischi <jay.zalischi@gmail.com>
2021-12-07 13:43:06 -0500

    installed more dependencies
    needed to install font awesome a different way because it is being used in react. Also had to install sass-loader and downgrade from node-sass version 7 to 6 to avoid error messsage.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Song from './components/Song';
+import './styles/app.scss';
+
 function App() {
   return (
     <div className='App'>
       <h1>Music Player</h1>
+      <Song />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Song = () => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default Song;
